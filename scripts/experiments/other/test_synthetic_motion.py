bd570e4e5e7f1889ce181f3d0a83417897365a3f
Dominik Lau <harek2388@gmail.com>
2025-10-01 09:45:26 +0200

    include left side in motion tests

---
+++ scripts/experiments/other/test_synthetic_motion.py
@@ -0,0 +1,37 @@
+from reconsnet.util.test import synthetic_test_adaptive
+from reconsnet.model.diffusion import DiffusionModule
+from reconsnet.data.dataset import XRayDatasetRight, XRayDatasetLeft, default_transform, random_move, random_scale
+
+DATA_PATH = "/home/shared/imagecas/projections_split/pilot"
+CHECKPOINT_RIGHT_PATH = "stronger-conditioning.ckpt"
+CHECKPOINT_LEFT_PATH = "left.ckpt"
+MODEL_RIGHT = DiffusionModule.load_from_checkpoint(CHECKPOINT_RIGHT_PATH, lr=1e-4)
+MODEL_LEFT = DiffusionModule.load_from_checkpoint(CHECKPOINT_LEFT_PATH, lr=1e-4)
+RECONSTRUCT_RIGHT = lambda x: MODEL_RIGHT.fast_reconstruct(*x, num_inference_steps=10, guidance=True)
+RECONSTRUCT_LEFT = lambda x: MODEL_LEFT.fast_reconstruct(*x, num_inference_steps=10, guidance=True)
+TRANSLATION_RANGE=20 # [px]
+SCALING_RANGE=0.2
+
+def motion(projections, gt):
+    projections[1].img = random_move(
+        random_scale(projections[1].img, SCALING_RANGE), 
+        TRANSLATION_RANGE
+    )
+    
+    return default_transform(projections, gt)
+
+
+synthetic_test_adaptive(
+    model=MODEL_RIGHT,
+    ds=XRayDatasetRight(root_dir=DATA_PATH, transform=motion),
+    csv_output_path="data/synthetic_motion_right.csv",
+    reconstruct=RECONSTRUCT_RIGHT
+)
+
+synthetic_test_adaptive(
+    model=MODEL_LEFT,
+    ds=XRayDatasetLeft(root_dir=DATA_PATH, transform=motion),
+    csv_output_path="data/synthetic_motion_left.csv",
+    reconstruct=RECONSTRUCT_LEFT
+)
+
