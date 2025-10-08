8adfd6f2f9c4143cd193b6479db46129c4c89bb8
Dominik Lau <harek2388@gmail.com>
2025-10-08 11:41:42 +0200

    add more experiments

--- scripts/experiments/synthetic/test_synthetic_diffusion_left.py
+++ scripts/experiments/synthetic/test_synthetic_diffusion_left.py
@@ -7,7 +7,7 @@ from reconsnet.data.dataset import XRayDatasetLeft
 
 CHECKPOINT_PATH = "left.ckpt"
 DATA_PATH = "/home/shared/imagecas/projections_split/pilot"
-RESULTS_PATH = "data/synthetic_diffusion_right.csv"
+RESULTS_PATH = "data/synthetic_diffusion_left.csv"
 MODEL = DiffusionModule.load_from_checkpoint(CHECKPOINT_PATH, lr=1e-4)
 RECONSTRUCT = lambda x: MODEL.fast_reconstruct(*x, num_inference_steps=10, guidance=True)
 
